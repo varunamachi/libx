450b85922443de34fdc3f9ae255ebc048c36c19e
Varuna Amachi <varunamachi@gmail.com>
2022-04-01 22:10:00 +0530

    Added funciton to remove spaces form a string

--- str/str.go
+++ str/str.go
@@ -1,6 +1,9 @@
 package str
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func EqFold(str string, targets ...string) bool {
 	for _, s := range targets {
@@ -9,4 +12,15 @@ func EqFold(str string, targets ...string) bool {
 		}
 	}
 	return false
+}
+
+func RemoveSpaces(str string) string {
+	var b strings.Builder
+	b.Grow(len(str))
+	for _, ch := range str {
+		if !unicode.IsSpace(ch) {
+			b.WriteRune(ch)
+		}
+	}
+	return b.String()
 }
