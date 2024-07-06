4004609024b10a79ac25dac68377a894fb5178c0
varunamachi <varunamachi@gmail.com>
2024-07-06 21:35:25 +0530

    added command builder, some email init changes

--- email/model.go
+++ email/model.go
@@ -1,8 +1,16 @@
 package email
 
 import (
+	"errors"
+
+	"github.com/varunamachi/libx/errx"
+	"github.com/varunamachi/libx/rt"
 	"github.com/varunamachi/libx/str"
 	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+var (
+	ErrInvalidMailProvider = errors.New("invalid mail provider")
 )
 
 type Message struct {
@@ -28,4 +36,24 @@ func (m *Message) SetContent(td *str.TemplateDesc) error {
 
 type Provider interface {
 	Send(desc *Message, html bool) error
+}
+
+func ProviderFromEnv(envPrefix string) (Provider, error) {
+	providerName := rt.EnvString(envPrefix+"_MAIL_PROVIDER", "")
+	if providerName == "" {
+		return nil, nil
+	}
+
+	switch providerName {
+	case envPrefix + "_SMTP_MAIL_PROVIDER":
+		return NewSMTPProviderFromEnv(envPrefix)
+	case envPrefix + "_FAKE_MAIL_PROVIDER":
+		return NewFakeEmailProvider(), nil
+	case envPrefix + "_SIMPLE_MAIL_SERVICE_CLIENT_PROVIDER":
+		return NewSimpleMailSrvClinetFromEnv(envPrefix)
+	default:
+		return nil, errx.Errf(ErrInvalidMailProvider,
+			"unknown mail provider name set as env variable: %s", providerName)
+
+	}
 }
--- rt/exec.go
+++ rt/exec.go
@@ -1,11 +1,73 @@
 package rt
 
-import "io"
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/varunamachi/libx/data"
+)
+
+// TODO - add way to start command in background
+// TODO - simple way to get output as string
 
 type CmdBuilder struct {
-	Env    map[string]any
-	Args   []string
-	Stdout io.Writer
-	Stdin  io.Reader
-	Stderr io.Writer
+	Cmd  string
+	Env  map[string]string
+	Args []string
+
+	stdout io.Writer
+	stdin  io.Reader
+	stderr io.Writer
+}
+
+func NewCmdBuilder(cmd string) *CmdBuilder {
+	return &CmdBuilder{
+		Cmd: cmd,
+	}
+}
+
+func (cb *CmdBuilder) WithEnv(name, value string) *CmdBuilder {
+	if cb.Env == nil {
+		cb.Env = map[string]string{}
+	}
+	cb.Env[name] = value
+	return cb
+}
+
+func (cb *CmdBuilder) WithArgs(args ...string) *CmdBuilder {
+	if cb.Args == nil {
+		cb.Args = args
+		return cb
+	}
+	cb.Args = append(cb.Args, args...)
+	return cb
+}
+
+func (cb *CmdBuilder) WithOutput(out, err io.Writer) *CmdBuilder {
+	cb.stdout, cb.stderr = out, err
+	return cb
+}
+
+func (cb *CmdBuilder) WithCombinedOutput(out io.Writer) *CmdBuilder {
+	cb.stdout, cb.stderr = out, out
+	return cb
+}
+
+func (cb *CmdBuilder) WithInput(in io.Reader) *CmdBuilder {
+	cb.stdin = in
+	return cb
+}
+
+func (cb *CmdBuilder) Execute() error {
+	cmd := exec.Command(cb.Cmd, cb.Args...)
+	cmd.Env = append(cmd.Env, cb.Args...)
+	cmd.Stdout = data.Qop(cb.stdout != nil, cb.stdout, io.Writer(os.Stdout))
+	cmd.Stderr = data.Qop(cb.stderr != nil, cb.stderr, io.Writer(os.Stderr))
+	cmd.Stdin = data.Qop(cb.stdin != nil, cb.stdin, io.Reader(os.Stdin))
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
