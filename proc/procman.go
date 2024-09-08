33db1cdee3f087031c52a2719adb6f04e9ef1aca
varunamachi <varunamachi@gmail.com>
2024-09-08 22:12:03 +0530

    wip - added more process manager stuff

--- proc/procman.go
+++ proc/procman.go
@@ -7,26 +7,73 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"sync"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/varunamachi/libx/data"
+	"github.com/varunamachi/libx/errx"
 )
 
-type cmd struct {
+type execdCmd struct {
 	command *exec.Cmd
 	desc    *CmdDesc
 }
 
 type Manager struct {
-	gtx  context.Context
-	cmds map[string]cmd
+	mutex sync.Mutex
+	gtx   context.Context
+	cmds  map[string]execdCmd
 }
 
-func (man *Manager) Add(cb *CmdDesc) (uint, error) {
+func (man *Manager) Add(cdesc *CmdDesc) (int, error) {
 
-	_ = man.mkcmd(cb)
+	cmd := man.mkcmd(cdesc)
 
-	return 0, nil
+	if err := cmd.Start(); err != nil {
+		return -1,
+			errx.Errf(err,
+				"failed to start command: %s - %s", cdesc.Name, cdesc.Path)
+	}
+	man.addToMap(cmd, cdesc)
+	go func() {
+		cmd.Wait()
+		man.removeFromMap(cdesc.Name)
+	}()
+
+	return cmd.Process.Pid, nil
+}
+
+func (man *Manager) IsRunning(name string) (bool, error) {
+	return false, nil
+}
+
+func (man *Manager) Terminate(name string, forceKill bool) error {
+	return nil
+}
+
+func (man *Manager) Get(name string) *exec.Cmd {
+	man.mutex.Lock()
+	defer man.mutex.Unlock()
+	excmd, found := man.cmds[name]
+	if !found {
+		return nil
+	}
+	return excmd.command
+}
+
+func (man *Manager) addToMap(cmd *exec.Cmd, desc *CmdDesc) {
+	man.mutex.Lock()
+	defer man.mutex.Unlock()
+	man.cmds[desc.Name] = execdCmd{
+		command: cmd,
+		desc:    desc,
+	}
+}
+
+func (man *Manager) removeFromMap(name string) {
+	man.mutex.Lock()
+	defer man.mutex.Unlock()
+	delete(man.cmds, name)
 }
 
 func (man *Manager) mkcmd(desc *CmdDesc) *exec.Cmd {
@@ -50,7 +97,6 @@ func (man *Manager) mkcmd(desc *CmdDesc) *exec.Cmd {
 		procNameStyle(),
 	)
 	return nil
-
 }
 
 func procNameStyle() lipgloss.Style {
